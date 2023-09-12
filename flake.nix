59d51252473345d11b0740ebe8a673402cedf561
Albert Chae <albertchae@users.noreply.github.com>
2023-09-12 16:28:14 -0700

    use yosys-ghdl from upstream

--- flake.nix
+++ flake.nix
@@ -46,36 +46,7 @@
           };
         });
 
-        yosys-ghdl = with final;
-          stdenv.mkDerivation {
-            pname = "yosys-ghdl";
-            # This is not the latest commit, but it's the latest that builds with current stable ghdl 2.0.0
-            version = "2022.01.11";
-
-            src = fetchFromGitHub {
-              owner = "ghdl";
-              repo = "ghdl-yosys-plugin";
-              rev = "c9b05e481423c55ffcbb856fd5296701f670808c";
-              sha256 = "sha256-tT2+DXUtbJIBzBUBcyG2sz+3G+dTkciLVIczcRPr0Jw=";
-            };
-
-            buildInputs = [ yosys readline zlib ghdl ];
-            nativeBuildInputs = [ pkg-config ];
-
-            doCheck = true;
-            installPhase = ''
-              mkdir -p $out/share/yosys/plugins
-              cp ghdl.so $out/share/yosys/plugins/ghdl.so
-            '';
-
-            meta = with lib; {
-              description = "GHDL plugin for Yosys";
-              homepage = "https://github.com/ghdl/ghdl-yosys-plugin";
-              license = licenses.isc;
-              platforms = platforms.all;
-              maintainers = with maintainers; [ thoughtpolice ];
-            };
-          };
+        yosys-ghdl = prev.yosys-ghdl;
 
         yosys = with final;
           stdenv.mkDerivation rec {
